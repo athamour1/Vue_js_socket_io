c6568b96a897b745fbd38922db73c72e906678f8
athamour1 <thanosmour.tk@gmail.com>
2021-04-25 10:49:00 +0300

    Update Server/app.js

---
+++ Server/app.js
@@ -0,0 +1,14 @@
+const Expreess = require("Express");
+const Htpp = require("http").Server(Express);
+const Socketio = require("socket.io")(Http);
+
+// i could use a database for a production application
+// Also the calculations happens in the server for "cheatting"
+var position = {
+    x: 200,
+    y: 200
+};
+
+Http.listen(3000, () => {
+    console.log("Listeninng at :3000...");
+});
